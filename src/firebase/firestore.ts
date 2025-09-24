28e381205ab867c9954cf91c8373dafe39817a50
Jeison Ferrer <jeison120120@hotmail.com>
2025-09-24 16:25:15 -0500

    fecha de creacion agregada a la inscripcion

--- src/firebase/firestore.ts
+++ src/firebase/firestore.ts
@@ -10,8 +10,10 @@ export const firestore = database;
 export const agregarInscripcion = async(inscripcion: Inscripcion) => {
 
     try {
+        const fechaActual = new Date();
         const docRef = await firestore.collection('inscripciones').add({
-            ...inscripcion
+            ...inscripcion,
+            fecha_creacion: fechaActual.toISOString()
         });
         return { success: true, id: docRef.id };
     } catch (error) {
